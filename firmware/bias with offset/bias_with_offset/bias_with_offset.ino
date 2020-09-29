f20fc108d1a2e97c6f2f75b6130b006ac635bf42
mgiacomelli <mgiacome@ur.rochester.edu>
2020-09-28 22:59:32 -0400

    Update bias_with_offset.ino
    Adds ability to save offset to Flash storage.  Also changes the serial port polling logic to not freeze so often when accessed from the .NET serial library.

--- firmware/bias with offset/bias_with_offset/bias_with_offset.ino
+++ firmware/bias with offset/bias_with_offset/bias_with_offset.ino
@@ -12,6 +12,7 @@ uint16_t offset = 250;
 bool on = LOW;
 uint8_t devAddr = 0x60;
 int eeAddress = 0;
+String cmd;
 
 Adafruit_MCP4728 mcp;
 
@@ -34,6 +35,7 @@ void setup()
     Serial.print("Found MCP4728 chip");
     mcp.setChannelValue(MCP4728_CHANNEL_B, 2048);
   }
+
 }
 
 void setVoltageSIPM( uint16_t output)
@@ -44,7 +46,8 @@ void setVoltageSIPM( uint16_t output)
     output = 2000;
   }
   
-  //note:  apd voltage is Vout*(R1/R2+1) where R1=1M and R2=20.5k
+  //note:  apd voltage is Vout*(R1/R2+1) where R1=1M and R2=18k
+  //for some reason the voltage seems to be a bit lower
    mcp.setChannelValue(MCP4728_CHANNEL_A, output, MCP4728_VREF_INTERNAL,
                       MCP4728_GAIN_1X);
 }
@@ -56,15 +59,32 @@ void setVoltageOffset( uint16_t output)
                       MCP4728_GAIN_1X);
 }
 
+
 void writeEEPROM(uint16_t offset)
 {
   EEPROM.update(eeAddress, offset);
-  EEPROM.commit();
+  EEPROM.update(eeAddress+1, offset>>8);
+ // EEPROM.commit();
 }
 
 void readEEPROM()
 {
-  offset = EEPROM.read(eeAddress);
+  //check if a sane value is present and if so use that as the offset
+
+  //uint16_t temp = EEPROM.read(eeAddress);
+  ////uint16_t temp2 = EEPROM.read(eeAddress+1);
+  //temp = temp+temp2<<8;
+  uint16_t temp = (EEPROM.read(eeAddress+1) << 8 ) | (EEPROM.read(eeAddress) & 0xff);
+  if(temp > 20 && temp < 1000){
+    offset = temp;
+    setVoltageOffset(offset);
+  }
+  else
+  {
+    char buf[30];
+    sprintf(buf, "Invalid EEPROM value: '%s'\n", temp);
+    Serial.print(buf);
+  }
 }
 
 void parse_command(char *str) {
@@ -80,62 +100,61 @@ void parse_command(char *str) {
     on = false;
   }
   else if (strcmp(tok, "on?") == 0) {
-    if (on) Serial.print("on\n");
-    else Serial.print("off\n");
+    if (on) Serial.write("on\n");
+    else Serial.write("off\n");
   }
   else if (strcmp(tok, "gain") == 0) {
     tok = strtok(NULL, " \n");
     long g = strtol(tok, NULL, 10);
     if (g > 4095 || g < 0) return;
     gain = (uint16_t)g;
+    setVoltageSIPM(gain);
   }
   else if (strcmp(tok, "gain?") == 0) {
     char buf[30];
     sprintf(buf, "%d\n", gain);
-    Serial.print(buf);
+    Serial.write(buf);
   }
   else if (strcmp(tok, "offset") == 0) {
     tok = strtok(NULL, " \n");
     long g = strtol(tok, NULL, 10);
     if (g > 4095 || g < 0) return;
     offset = (uint16_t)g;
+    setVoltageOffset(offset);
   }    
   else if (strcmp(tok, "offset?") == 0) {
     char buf[30];
     sprintf(buf, "%d\n", offset);
-    Serial.print(buf);
+    Serial.write(buf);
   }
-  else if (strcmp(tok, "write offset") == 0) {
+  else if (strcmp(tok, "write_offset") == 0) {
     writeEEPROM(offset);
   }
-  else if (strcmp(tok, "read offset") == 0) {
-     Serial.print("reading offset");
+  else if (strcmp(tok, "read_offset") == 0) {
+     Serial.write("reading offset\n");
     readEEPROM();
   }  
   else if (strcmp(tok, "name?") == 0) {
-    Serial.print(NAME);
+    Serial.write(NAME);
   }
   else if(strlen(tok)>2)
   {
     char buf[30];
     sprintf(buf, "Unknown command: '%s'\n", tok);
-    Serial.print(buf);
+    Serial.write(buf);
   }
 }
 
 void loop() 
 {
-  String cmd = Serial.readString();
-  char *str = (char*)cmd.c_str();
+  if(Serial.available()){
+    cmd = Serial.readStringUntil('\n');
+    char *str = (char*)cmd.c_str();
+    parse_command(str);
+  }
 
-  parse_command(str);
-
-  delay(100);
+  delay(30);
   digitalWrite(13, on);
   
-  setVoltageSIPM(gain);
-  setVoltageOffset(offset);
-
-
 
 }
